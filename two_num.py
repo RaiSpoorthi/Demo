b70872c996307ddb6b63354f5fa45767f2a0fa13
Spoo <spoorthirai61@gmail.com>
2024-06-22 15:39:01 +0530

    adding

---
+++ two_num.py
@@ -0,0 +1,10 @@
+# Function to add two numbers
+def add_numbers(a, b):
+    return a + b
+
+# Example usage
+number1 = 5
+number2 = 3
+
+result = add_numbers(number1, number2)
+print("The sum of", number1, "and", number2, "is:", result)
